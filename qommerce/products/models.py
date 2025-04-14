01f6dfd4391476f04bd7113fa47c52bcc0c993cb
aaaaryaaa <aarya.masters@gmail.com>
2025-04-14 18:22:20 +0530

    filtering and wishlist added

--- qommerce/products/models.py
+++ qommerce/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -24,3 +25,14 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+    
+class Wishlist(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    added_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ['user', 'product']
+    
+    def __str__(self):
+        return f"{self.user.username}'s wishlist item: {self.product.name}"
